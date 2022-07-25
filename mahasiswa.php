981b8e93db8609b055fb52200151818d592954f0
Gilang Lazuardi <gilanglazu@gmail.com>
2022-07-25 12:21:48 +0700

    menambah mahasiswa.php

---
+++ mahasiswa.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mahasiswa</title>
+</head>
+<body>
+<h2>Mahasiswa</h2>
+</body>
+</html>
